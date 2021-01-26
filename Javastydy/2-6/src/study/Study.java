8e630ccef50913a7c753dae800cd11c7174d5d6e
sigeki-ryota <73868435+sigeki-ryota@users.noreply.github.com>
2021-01-26 10:27:54 +0900

    Update Study.java

--- Javastydy/2-6/src/study/Study.java
+++ Javastydy/2-6/src/study/Study.java
@@ -17,6 +17,10 @@ public class Study {
 
     public static void main(String args[]) {
 
+
+            Calendar cal = Calendar.getInstance();
+            System.out.println(cal.getTime());
+
         // Dateクラスのインスタンスを生成
         Date now = new Date();
 
@@ -34,15 +38,13 @@ public class Study {
         // 問③ 上記の「calendar」を使用し、本日から1年2ヶ月15日先の日付を表示しなさい。
         // - この課題に関しては課題実施日によって結果が異なりますので、Wiki課題の画像とは結果が異なります。
         // - また、うるう年の場合は設定内容が正しくとも日付がズレて表示される可能性があります。
-        calendar.add(Calendar.YEAR, 1);
-        System.out.println(calendar.get(Calendar.YEAR));
-        
-        calendar.add(Calendar.MONTH, 3);
-        System.out.println(calendar.get(Calendar.MONTH));
-        
+        System.out.println(calendar.get(Calendar.YEAR)+1);
+
+        System.out.println(calendar.get(Calendar.MONTH)+3);
+
         calendar.add(Calendar.DATE, 15);
         System.out.println(calendar.get(Calendar.DATE));
-        
+
      // うるう年判定
         printLeapYear(calendar.get(Calendar.YEAR));
     }
@@ -58,4 +60,3 @@ public class Study {
 }
 
 
-      
